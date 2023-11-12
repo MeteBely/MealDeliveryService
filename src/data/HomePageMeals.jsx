f704a2681dc91853cf447787613329b470a4ac9e
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-12 22:23:06 +0300

    Home sayfasındaki meallerin datasının tutulması

---
+++ src/data/HomePageMeals.jsx
@@ -0,0 +1,55 @@
+const HomePageMeals = [
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/craft.webp?width=300&amp;height=300&amp;quality=90',
+        title:'CRAFT',
+        description:'Cheese Crisp Burgers'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/wellness.webp?width=300&amp;height=300&amp;quality=90',
+        title:'WELLNESS',
+        description:'Turkey & Mushroom Lettuce Cups'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/family-friendly.webp?width=300&amp;height=300&amp;quality=90',
+        title:'FAMILY FRIENDLY',
+        description:'Mafalda Pasta'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/fast-and-easy.webp?width=300&amp;height=300&amp;quality=90',
+        title:'FAST & EASY',
+        description:'Sheet Pan Cheesy Jalapeño Chicken'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/vegetarian.webp?width=300&amp;height=300&amp;quality=90',
+        title:'VEGETARIAN',
+        description:'Quinoa & Vegetable “Fried Rice”'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/premium-steak.webp?width=300&amp;height=300&amp;quality=90',
+        title:'PREMIUM',
+        description:'NY Strip Steaks & Herb-Mushroom Pan Sauce'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/ready-to-cook.webp?width=300&amp;height=300&amp;quality=90',
+        title:'READY TO COOK',
+        description:'Pesto Chicken and Orzo'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/heat-and-eat.webp?width=300&amp;height=300&amp;quality=90',
+        title:'HEAT & EAT',
+        description:'Cheesy Truffle Cavatappi'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/breakfast.webp?width=300&amp;height=300&amp;quality=90',
+        title:'BREAKFAST',
+        description:'Fried Egg & Prosciutto Sandwiches'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/desserts.webp?width=300&amp;height=300&amp;quality=90',
+        title:'DESSERTS',
+        description:'Flourless Chocolate Cake'
+    }
+]
+
+export default HomePageMeals
+
