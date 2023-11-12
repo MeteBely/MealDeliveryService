003274ca65ab91d1476e7c9d7549adea57737fe1
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-12 22:23:30 +0300

    Neden kullanılmalı objelerinin datada tutulması

---
+++ src/data/WhyUsingThis.jsx
@@ -0,0 +1,22 @@
+const WhyUsingThis = [
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/chef-experience.webp?height=374&quality=90',
+        title:'5 decades of top restaurant experience',
+        descriptionOne:'Our chefs bring high standards to',
+        descriptionTwo:'crafting your meals.'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/fresh-ingredients.webp?height=374&quality=90',
+        title:'Fresher ingredients faster',
+        descriptionOne:'80% of ingredients come directly from',
+        descriptionTwo:'producers.'
+    },
+    {
+        src:'https://media.blueapron.com/assets/registration/homepage/flexible-mobile.webp?height=374&quality=90',
+        title:'You\'re busy, so we\'re flexible',
+        descriptionOne:'Get boxes on your schedule. Skip,',
+        descriptionTwo:'pause, or cancel anytime.'
+    }
+]
+
+export default WhyUsingThis
