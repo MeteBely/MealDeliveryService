8856be992735d8216e9e26c6ea2c6748860c6052
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-12 22:19:03 +0300

    App.jsx düzenlemesi
    App.jsx'e header, home ve footer componentlerinin geçilmesi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
         <Header/>
+        <Home/>
+        <Footer/>
     </>
   )
 }
