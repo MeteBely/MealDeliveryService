86a03d6f129aec50386b40a423d2d558d406d030
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-13 21:33:23 +0300

    header componentinde hover ve font işlemleri

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 
 const Header = () => {
   return (
@@ -7,15 +8,15 @@ const Header = () => {
              <div className='navbar flex justify-evenly items-center w-full h-16 tracking-widest'>
                 <div className='navbarItemsOne flex flex-row gap-8 text-coolGray text-xs font-normal items-center'>
                     <a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Blue_Apron_logo.svg/1280px-Blue_Apron_logo.svg.png" className='w-24 h-11 inline-block' /></a>
-                    <a href="">PLANS</a>
-                    <a href="">ON THE MENU</a>
-                    <a href="">MARKET</a>
-                    <a href="">GIFT CARDS</a>
-                    <a href="">BLOG</a>
+                    <a href="" className='hover:text-[#0f346c]'>PLANS</a>
+                    <a href="" className='hover:text-[#0f346c]'>ON THE MENU</a>
+                    <a href="" className='hover:text-[#0f346c]'>MARKET</a>
+                    <a href="" className='hover:text-[#0f346c]'>GIFT CARDS</a>
+                    <a href="" className='hover:text-[#0f346c]'>BLOG</a>
                 </div>
                 <div className='navbarItemsTwo flex flex-row gap-4 items-center text-coolGray tracking-widest'>
-                    <a href="" className='font-normal text-xs'>LOG IN</a>
-                    <a href="" className='text-sm font-semibold bg-orange-500 text-white w-28 h-10 text-center pt-2.5 rounded'>SIGN UP</a>
+                    <a href="" className='font-normal text-xs hover:text-[#0f346c]'>LOG IN</a>
+                    <a href="" className='fontCera text-sm bg-orange-500 text-white w-28 h-10 text-center pt-2.5 rounded hover:bg-[#FF8142]'>SIGN UP</a>
                 </div>
              </div>
         </nav>
