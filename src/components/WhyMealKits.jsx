b16005fb23a77baaf4380e801b846efa8ba1180c
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-13 21:34:09 +0300

    font işlemleri

--- src/components/WhyMealKits.jsx
+++ src/components/WhyMealKits.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import '../App.css'
 
 const WhyMealKits = ({kitBox}) => {
   return (
     <div className='kitBox'>
       <img src={kitBox.src} alt="" />
-      <p className='text-[15px] font-bold text-[#303236] text-center mb-[12px] mt-4'>{kitBox.title}</p>
-      <p className='text-[15px] text-center text-[#303236] font-normal'>{kitBox.descriptionOne}</p>
-      <p className='text-[15px] text-center text-[#303236] font-normal'>{kitBox.descriptionTwo}</p>
+      <p className='fontCera text-[15px] font-bold text-[#303236] text-center mb-[12px] mt-4'>{kitBox.title}</p>
+      <p className='fontCera text-[15px] text-center text-[#303236] '>{kitBox.descriptionOne}</p>
+      <p className='fontCera text-[15px] text-center text-[#303236] '>{kitBox.descriptionTwo}</p>
     </div>
   )
 }
