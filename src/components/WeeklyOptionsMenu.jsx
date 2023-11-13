652eeee1457c87324e6f56dbdc7c18feff1113a2
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-13 21:34:05 +0300

    font işlemleri

--- src/components/WeeklyOptionsMenu.jsx
+++ src/components/WeeklyOptionsMenu.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '../App.css'
 
 const WeeklyOptionsMenu = ({mealBox}) => {
   return (
     <div className='mealBoxDiv'>
       <img src={mealBox.src} className='w-[340px]'/>
-      <p className='mealTitle text-[13px] font-bold tracking-[1.3px] text-[#002c9b] text-center mt-2'>{mealBox.title}</p>
-      <p className='mealSubtitle text-[15px] font-medium text-[#6a6d75] text-center mb-3'>{mealBox.description}</p>
+      <p className='fontCera mealTitle text-[13px] font-bold tracking-[1.3px] text-[#002c9b] text-center mt-2'>{mealBox.title}</p>
+      <p className='fontCera mealSubtitle text-[15px] text-[#6a6d75] text-center mb-3'>{mealBox.description}</p>
     </div>
   )
 }
