069dc459cdb77b37e1407f450520fe81b5f8a79a
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-14 23:33:51 +0300

    FrequentlyAskedQuestions adlı dizi oluşturulup export edilmesi

---
+++ src/data/FrequentlyAskedQuestions.jsx
@@ -0,0 +1,48 @@
+const FrequentlyAskedQuestions = [
+    {
+        title:'Does Blue Apron charge weekly?',
+        answer:'Your payment information will be charged approximately 3 to 5 days before your delivery date, unless you decide to skip an order prior to the cutoff date.'
+    },
+    {
+        title:'How much is Blue Apron?',
+        answer:'The cost of our plans varies depending on the number of servings you order and the type of meal kit. Find more specific information about our prices above.'
+    },
+    {
+        title:'When does Blue Apron deliver?',
+        answer:'Once you create an account, you’ll have the ability to choose which day of the week your Blue Apron meal kit arrives and adjust it each week as needed. In many locations, we deliver 7 days of the week. Once your order has shipped, you’ll receive an email notification that includes your order details and tracking number.'
+    },
+    {
+        title:'Where does Blue Apron deliver?',
+        answer:'We deliver to all locations in the continental United States.'
+    },
+    {
+        title:'Do I need to be home to accept my delivery?',
+        answer:'Our refrigerated boxes are designed with ice packs and insulated liners to help ensure your ingredients stay fresh, even if you’re not at home to accept the delivery. You should place all perishable items in the refrigerator upon delivery or immediately when you get home. If you ever have any questions about the safe handling of your food, please contact us at contact@blueapron.com.'
+    },
+    {
+        title:'Can I skip my delivery for a particular week?',
+        answer:'Yes, you can skip upcoming deliveries up to 5 weeks in advance and skip any delivery until the order is processed (3-5 days before delivery date). You will receive a reminder before your order processes each week. To manage your deliveries, sign into your account and head to your Upcoming page.'
+    },
+    {
+        title:'Can I send Blue Apron as a gift?',
+        answer:'Yes! You can purchase an e-gift card in any amount that you\'d like, applicable towards weekly recurring deliveries on any meal plan. To purchase a meals gift card, go to our Gifts page and select “Meal” for “Gift Type.” We also have Blue Apron Wine e-gift cards available for purchase.'
+    },
+    {
+        title:'Does Blue Apron offer diet-friendly or allergy-friendly recipes?',
+        answer:'We accommodate a variety of dietary preferences in our recipes including vegetarian, carb conscious, and 600 calories or less. If you don’t eat meat, you can select from our fish, seafood, or poultry offerings and personalize your menu each week based on your preferences. Allergens are designated in the online Nutrition Facts Panel for each recipe that can be viewed at your point-of-purchase and on the package label of ingredients that we deliver in your box. Note that all of our boxes are assembled in a facility that processes all eight major food allergens. Check with your healthcare provider to determine if Blue Apron is suitable for your lifestyle.'
+    },
+    {
+        title:'Where does Blue Apron source ingredients?',
+        answer:'The quality and freshness of our ingredients are incredibly important to us, so we work directly with reputable suppliers that support sustainable practices and engage with artisanal purveyors and family-run farms for some of our specialty ingredients.'
+    },
+    {
+        title:'How do I cancel my Blue Apron delivery?',
+        answer:'Blue Apron is flexible and lets you skip weeks when you want, prior to each order being processed. To skip, simply sign in to your account and click ‘Manage Delivery’ on the order you\'d like to skip. If you’d like to cancel your Blue Apron account altogether, you can contact our Customer Experience team and we\'ll provide you instructions on how to complete the process'
+    },
+    {
+        title:'Can I recycle Blue Apron boxes?',
+        answer:'Over 85% of Blue Apron packaging is recyclable by weight. We’ve teamed up with How2Recycle® to implement their industry-approved, easy-to-identify recycling label program that helps reduce the amount of recyclable material in our waste stream.'
+    },
+]
+
+export default FrequentlyAskedQuestions
