40a65cab721951b977542acb46a5843b850648bc
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-14 23:33:39 +0300

    MealPreferences adlı dizi oluşturulup export edilmesi

---
+++ src/data/MealPreferences.jsx
@@ -0,0 +1,26 @@
+const MealPreferences = [
+    {
+        title:'Chef Favorites',
+        description:'Our test kitchen\'s top picks'
+    },
+    {
+        title:'Wellness',
+        description:'Nutritionist-approved recipes that don\'t sacrifice flavor'
+    },
+    {
+        title:'Family Friendly',
+        description:'Classic flavors the whole family will love'
+    },
+    {
+        title:'Fast & Easy',
+        description:'Recipes designed for low prep or quick cook times'
+    },
+    {
+        title:'Veggies',
+        description:'Meat-free meals highlighting seasonally inspired produce'
+    }
+]
+
+
+
+export default MealPreferences
