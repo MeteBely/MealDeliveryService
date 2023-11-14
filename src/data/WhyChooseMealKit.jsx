db4143c7f83f8364b64ce3a9413d3f2caf1a7d1a
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-14 23:33:10 +0300

    WhyChooseMealKit adlı dizi oluşturulup export edilmesi

---
+++ src/data/WhyChooseMealKit.jsx
@@ -0,0 +1,29 @@
+const WhyChooseMealKit = [
+    {
+        title:'Well-balanced meals',
+        description:'Choose from an ever-changing mix of menu options featuring meat, poultry, fish, seafood and more.'
+    },
+    {
+        title:'Chef-approved recipes',
+        description:'Our Test Kitchen chefs are in pursuit of dishes that are not just good but special, bringing something extraordinary to your home cooking experience.'
+    },
+    {
+        title:'Your plan, your way',
+        description:'Cooking with us is always on your terms. Adjust your servings and delivery day based on that week\'s needs. Need to skip a week or cancel? No problem. As long as your order hasn\'t been processed, you won\'t be charged.'
+    },
+    {
+        title:'Tell us what you think',
+        description:'Our culinary team reads every review and creates recipes based on your ratings. Tell us what you like and what you don\'t on the app. (Don\'t worry, our chefs can take the heat!)'
+    },
+    {
+        title:'A more sustainable way to cook',
+        description:'When you cook with Blue Apron, you\'re doing more than making delicious meals. Our meal kits include perfectly portioned ingredients to cut down on food waste in your home.'
+    },
+    {
+        title:'We\'re sticklers for quality',
+        description:'Our team of experts aim to source from suppliers that meet our standards for sustainable farming practices, high-quality produce, and unique ingredients.'
+    },
+]
+
+
+export default WhyChooseMealKit
