de7edc226f96a095c3e47c6e9829e00fe3258371
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-15 23:15:06 +0300

    Market içerisindeki MealKit verilerini data'da tuttuk.

---
+++ src/data/MealKitsDatas.jsx
@@ -0,0 +1,131 @@
+const MealKitsDatas = [
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002320/attachment/f253600a44a1d692c1a55952ab99d740.jpg?width=400',
+        title:'Signature Meal Kit',
+        description:'SHIPS NEXT DAY! Includes 2 recipes, 2 servings each',
+        price:'$60.95'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002324/attachment/8f3936153c11a891df0e3fcb7157c583.jpg?width=400',
+        title:'Premium Meal Kit',
+        description:'Includes 3 recipes, 2 servings each',
+        price:'$124.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002321/attachment/462a7bedd42439f92155347fef026e3a.jpg?width=400',
+        title:'Easy Prep Dinner Meal Kit',
+        description:'SHIPS NEXT DAY! Includes 2 recipes, 2 servings each',
+        price:'$60.95'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002322/attachment/cff20076dc1d02c75cd26591ea7b7653.jpg?width=400',
+        title:'Ready to Cook Meal Kit',
+        description:'SHIPS NEXT DAY! Includes 2 recipes, 2 servings each',
+        price:'$60.95'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002323/attachment/c5a177356d9d3f0c4d6d05392daa1ec2.jpg?width=400',
+        title:'Fast & Delicious Meal Kit',
+        description:'Includes 3 recipes, 2 servings each',
+        price:'$79.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002325/attachment/225607960dc30504af02112c41a2e136.jpg?width=400',
+        title:'Customer Favorites Meal Kit',
+        description:'Includes 3 recipes, 2 servings each',
+        price:'$79.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002328/attachment/c8723bb5132f8c4d203b231391ee8ce8.jpg?width=400',
+        title:'Wellness Meal Kit',
+        description:'Includes 3 recipes, 2 servings each',
+        price:'$79.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002326/attachment/8395462ac6b41601840cbf0b64507a4a.jpg?width=400',
+        title:'Fast & Delicious Family Meal Kit',
+        description:'Includes 2 recipes, 4 servings each',
+        price:'$94.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002327/attachment/76d3eda239c77ce53c2d8b1948c684bd.jpg?width=400',
+        title:'Pescatarian Meal Kit',
+        description:'Includes 3 recipes, 2 servings each',
+        price:'$79.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002282/attachment/9d8396d9c672fc88efc8fd1f32c7f1e1.jpg?width=400',
+        title:'Holiday Roast Box',
+        description:'Preorder now for delivery the week of November 27th',
+        price:'$181.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002298/attachment/51756dec7514dc2931be6d01805b088d.jpg?width=400',
+        title:'Classic Thanksgiving Box',
+        description:'with Roasted Turkey Breast and Homemade Gravy, Sides & Dessert',
+        price:'$131.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002299/attachment/10fa23acdaa5dc348d3ab978ee6e72cd.jpg?width=400',
+        title:'Vegetarian Holiday Box',
+        description:'with Three-Cheese Cavatappi Pasta Bake, Sides & Dessert',
+        price:'$121.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002300/attachment/709452f257d5aeaa3553368fb3256735.jpg?width=400',
+        title:'Holiday Ham Box',
+        description:'with Baked Ham and Brown Sugar, Sides & Dessert',
+        price:'$161.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002290/attachment/4c41e580494f890ddcd66dd4e6c68df4.jpeg?width=400',
+        title:'Mexican-Style Chicken & Rice Bowl Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Mexican-Style Chicken & Rice Bowl with Pinto Beans & Vegetables',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002288/attachment/b7cd2c68407373731b76c9fbacdf7d58.jpeg?width=400',
+        title:'Mac & Cheese Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Mac & Cheese with Pulled Pork, Peppers & Corn',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002286/attachment/f4440fa535ad4d102d0414d90c992aa2.jpeg?width=400',
+        title:'Cheesy Truffle Cavatappi Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Cheesy Truffle Cavatappi with Mushrooms & Kale',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002287/attachment/ad2a1bb6347fce609309c002adba546a.jpeg?width=400',
+        title:'Soy-Miso Chicken & Udon Noodles Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Soy-Miso Chicken & Udon Noodles with Bok Choy & Mushrooms',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002285/attachment/09b2b5229e423fd35a6ddb8e53d0b850.jpeg?width=400',
+        title:'Four-Cheese Enchiladas Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Four-Cheese Enchiladas with Rice & Vegetables',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002283/attachment/6061ed5f213b9cb96bc21de718a7444f.jpeg?width=400',
+        title:'Spicy Cavatappi Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Spicy Cavatappi & Beef Bolognese with Parmesan & Ricotta Cheese',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002284/attachment/8d17bd74ae58d26dfe538df14b6c1e5c.jpeg?width=400',
+        title:'Egg Noodles & Beef Meatballs Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Egg Noodles & Beef Meatballs with Creamy Mushroom Sauce & Peas',
+        price:'$54.99'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002289/attachment/2d7f94701bcc0cb46797dda3bef0ee6c.jpeg?width=400',
+        title:'Lemon Chicken Heat & Eat Meals (4 Pack)',
+        description:'SHIPS NEXT DAY! Lemon Chicken with Brown Rice, Asparagus & Peas',
+        price:'$54.99'
+    },
+]
+
+
+export default MealKitsDatas
