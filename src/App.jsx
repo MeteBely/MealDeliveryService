062a21e6aa83ba0f15b091032e94d421b64424b2
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-15 23:07:12 +0300

    React router dom işlemleri
    Child componentleri özenle yerleştirdik. Outlet yapısından yararlandık. Layout mantığını kavradık. Element vermeye gerek olmadığını kavradık.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet} from 'react-router-dom'
 import Pricing from './components/Pricing'
+import Market from './components/Market'
+import MealKits from './components/MealKits'
+import SeasonalBoxes from './components/SeasonalBoxes'
 
 function App() {
 
@@ -13,6 +16,14 @@ function App() {
         <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/pricing' element={<Pricing/>}/>
+        <Route path='/market'>
+            <Route index={true} element={<Market/>}/>
+            <Route path='categories'>
+              <Route index={true} element={<Outlet/>}/>
+              <Route path='seasonal-boxes' element={<SeasonalBoxes/>}/>
+              <Route path='meal-kits' element={<MealKits/>}/>
+            </Route>
+        </Route>
         </Routes>
         <Footer/>
     </>
