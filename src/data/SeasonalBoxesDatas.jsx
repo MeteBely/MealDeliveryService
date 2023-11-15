062596d244e92ca8fea23e235867642d81dcd656
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-15 23:14:02 +0300

    Market içerisindeki Seasonal Boxes içerisindeki Meals verilerini data'da tuttuk.

---
+++ src/data/SeasonalBoxesDatas.jsx
@@ -0,0 +1,28 @@
+const SeasonalBoxesDatas = [
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002282/attachment/9d8396d9c672fc88efc8fd1f32c7f1e1.jpg?width=400',
+        title:'Holiday Roast Box',
+        description:'Preorder now for delivery the week of November 27th',
+        price:'$181.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002298/attachment/51756dec7514dc2931be6d01805b088d.jpg?width=400',
+        title:'Classic Thanksgiving Box',
+        description:'with Roasted Turkey Breast and Homemade Gravy, Sides & Dessert',
+        price:'$131.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002299/attachment/10fa23acdaa5dc348d3ab978ee6e72cd.jpg?width=400',
+        title:'Vegetarian Holiday Box',
+        description:'with Three-Cheese Cavatappi Pasta Bake, Sides & Dessert',
+        price:'$121.98'
+    },
+    {
+        src:'https://media.blueapron.com/agora/images/product/CCMK0002300/attachment/709452f257d5aeaa3553368fb3256735.jpg?width=400',
+        title:'Holiday Ham Box',
+        description:'with Baked Ham and Brown Sugar, Sides & Dessert',
+        price:'$161.98'
+    }
+]
+
+export default SeasonalBoxesDatas
