de2267b2dfa21516b4745867986af386a737d83f
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-17 00:36:48 +0300

    Sepet componentinden vazgeçilmesi

--- src/components/GiftCards.jsx
+++ src/components/GiftCards.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import whiteBlueApronIcon from '../assets/whiteBlueApronIcon.png'
-import YourGiftCart from './YourGiftCart'
 
 const GiftCards = () => {
 
