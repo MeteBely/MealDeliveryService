4e72e052cbadcff07f7e2743ce8bdefff652a92c
MeteBely <metehanmuradoglu17@gmail.com>
2023-11-21 10:22:32 +0300

    CustomInput error stil işlemi

--- src/components/FormComponents/CustomInput.jsx
+++ src/components/FormComponents/CustomInput.jsx
@@ -7,7 +7,7 @@ const CustomInput = ({label, ...props}) => {
     <div className='mb-[20px] fontCera w-full'>
         <label className='text-[13px] mb-[5px] fontCera tracking-widest'>{label}</label>    
         <input {...field} {...props} className='border block border-[#d3d5db] h-[40px] rounded-[3px] px-[10px] w-full CoSaFormInput'/>{/*tailwind kullandığın için className={meta.error ? 'input-error' : ''} kaldırdın. */}
-        {meta.error && <div className='error'>{meta.error}</div>}
+        {meta.error && <div className='text-[14px] bg-[#d64148] fontCera py-[9px] px-[10px] text-[#fff] leading-[21px] rounded-b-[4px]'>{meta.error}</div>}
     </div>
   )
 }
