cb83c9a5009d541b99e151480830315eaee93d89
MeteBely <metehanmuradoglu17@gmail.com>
2023-12-16 21:17:39 +0300

    on-the-menu componentinin datalarının kaydı

---
+++ src/data/onTheMenuDatas/WellnessTwoServing.jsx
@@ -0,0 +1,54 @@
+//!İLK HAFTA
+export const WellnessTwoServingFirstWeek = [
+  {
+    img: '',
+    type: '',
+    name: '',
+    detail: '',
+    time: '',
+  },
+];
+
+//! İKİNCİ HAFTA
+export const WellnessTwoServingSecondWeek = [
+  {
+    img: '',
+    type: '',
+    name: '',
+    detail: '',
+    time: '',
+  },
+];
+
+//! ÜÇÜNCÜ HAFTA
+export const WellnessTwoServingThirdWeek = [
+  {
+    img: '',
+    type: '',
+    name: '',
+    detail: '',
+    time: '',
+  },
+];
+
+//! DÖRDÜNCÜ HAFTA
+export const WellnessTwoServingfourthWeek = [
+  {
+    img: '',
+    type: '',
+    name: '',
+    detail: '',
+    time: '',
+  },
+];
+
+//! SON HAFTA
+export const WellnessTwoServingFifthWeek = [
+  {
+    img: '',
+    type: '',
+    name: '',
+    detail: '',
+    time: '',
+  },
+];
