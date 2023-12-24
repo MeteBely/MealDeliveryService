db35cbee53c97cb9ef035544daca9d2546a1dbd9
MeteBely <metehanmuradoglu17@gmail.com>
2023-12-24 15:07:07 +0300

    weekofmonth componentinde background color düzenlemesi

--- src/components/WeekOfMonth.jsx
+++ src/components/WeekOfMonth.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const WeekOfMonth = ({ date }) => {
   return (
-    <div className="w-full flex flex-row justify-center items-center gap-[675px] selectMenuBox pb-[5px]">
+    <div className={`w-full flex flex-row justify-center items-center gap-[675px] selectMenuBox pb-[5px] bg-white`}>
       <span className="tracking-[.27em] text-[#002c9b] text-[18px] fontCera font-semibold">{date}</span>
       <button className="fontCera text-[14px] rounded-[30px] h-[45px] px-[33px] w-[180px] bg-blue-700 text-white tracking-[2px] font-semibold">SEE PLANS</button>
     </div>
