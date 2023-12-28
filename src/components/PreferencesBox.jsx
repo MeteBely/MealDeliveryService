863a992a76c8833d24e7c281891183af440c3dff
MeteBely <metehanmuradoglu17@gmail.com>
2023-12-29 00:45:07 +0300

    menü box'ın click ve icon olaylarının tamamlanması

--- src/components/PreferencesBox.jsx
+++ src/components/PreferencesBox.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react';
+import tick from './../assets/tick.png';
 
 // eslint-disable-next-line react/prop-types
-const PreferencesBox = ({ title, description, img }) => {
+const PreferencesBox = ({ title, description, img, imgBlue }) => {
   const [mouseOnBox, setMouseOnBox] = useState(false);
+  const [mouseOnClick, setMouseOnClick] = useState(false);
 
   return (
-    <button onMouseOver={() => setMouseOnBox(true)} onMouseOut={() => setMouseOnBox(false)} className={`w-[380px] border-2 border-[#d3d6cd]  h-[84px] rounded-md flex flex-row justify-start items-center gap-4 box-content transition-all duration-200 ease-linear hover:border-[#002684] hover:border-[2px]`}>
+    <button onClick={() => setMouseOnClick(!mouseOnClick)} onMouseOver={() => setMouseOnBox(true)} onMouseOut={() => setMouseOnBox(false)} className={`relative w-[380px] border-2 h-[84px] rounded-md flex flex-row justify-start items-center gap-4 box-content transition-all duration-200 ease-linear hover:border-[#002684] hover:border-[2px] ${mouseOnClick ? 'border-[#002684] ' : 'border-[#d3d6cd]'}`}>
       <div className="ml-[16px]">
-        <img src={img} alt="" className="h-[40px] w-[40px] mr-2" />
+        <img src={`${mouseOnClick || mouseOnBox ? imgBlue : img}`} alt="" className="h-[40px] w-[40px] mr-2" />
       </div>
       <div>
-        <p className={`text-[16px] text-[#303236] text-left transition-all duration-200 ease-linear ${mouseOnBox === true ? 'text-[#012684]' : ''}`}>{title}</p>
+        <p className={`text-[16px] text-left transition-all duration-200 ease-linear ${mouseOnBox || mouseOnClick ? 'text-[#002684]' : 'text-[#303236]'}`}>{title}</p>
         <p className="text-[#6a6d75] text-[15px] leading-[16px] text-left">{description}</p>
       </div>
+      <img src={tick} className={`${mouseOnClick ? 'absolute top-2 right-2 inline-block w-[21px] h-[21px]' : 'hidden'}`}></img>
     </button>
   );
 };
