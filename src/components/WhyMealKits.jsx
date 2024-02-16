f587b1b22d4beb17aa555f8b72567c4e6e1c143e
MeteBely <metehanmuradoglu17@gmail.com>
2024-02-17 00:22:18 +0300

    stil düzeltmesi

--- src/components/WhyMealKits.jsx
+++ src/components/WhyMealKits.jsx
@@ -3,11 +3,10 @@ import '../App.css';
 
 const WhyMealKits = ({ kitBox }) => {
   return (
-    <div className="kitBox fontCera text-[15px] text-center text-[#303236]">
+    <div className="kitBox fontCera md:text-[15px] text-[12px] text-center text-[#303236] flex flex-col justify-center items-center">
       <img src={kitBox.src} alt="" />
-      <p className="font-bold mb-[12px] mt-4">{kitBox.title}</p>
-      <p>{kitBox.descriptionOne}</p>
-      <p>{kitBox.descriptionTwo}</p>
+      <p className="font-bold mb-[16px] mt-4 h-8">{kitBox.title}</p>
+      <p className="hidden sm:block h-4">{kitBox.descriptionOne}</p>
     </div>
   );
 };
