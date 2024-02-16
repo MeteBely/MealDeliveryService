ee86003ca77dd927cf1bcc7549108a57a438cd16
MeteBely <metehanmuradoglu17@gmail.com>
2024-02-17 00:22:39 +0300

    home page first section res yapım

--- src/components/HomeFirstSection.jsx
+++ src/components/HomeFirstSection.jsx
@@ -1,19 +1,20 @@
+import '../App.css';
 const HomeFirstSection = () => {
   return (
     <section>
       <div className="relative">
-        <img className="w-full h-[670px] object-cover" src="https://media.blueapron.com/assets/registration/homepage/desktop-splash-v2.webp?height=800&quality=90&format=pjpg" alt="" />
-        <h1 className="fontChronicle absolute top-2/4 left-10 text-5xl text-blue-700">
+        <img className="w-full h-[670px] object-cover object-left" src="https://media.blueapron.com/assets/registration/homepage/desktop-splash-v2.webp?height=800&quality=90&format=pjpg" alt="" />
+        <h1 className="fontChronicle absolute md:top-2/4 top-1/4 md:left-10 left-[16%]  text-5xl text-blue-700">
           The meal kit that
           <br />
           puts quality first
         </h1>
-        <button className="fontCera absolute top-2/3 left-10 mt-5 rounded-3xl	h-[50px] w-[180px] bg-blue-700 text-white tracking-widest">SEE PLANS</button>
+        <button className="fontCera absolute md:top-2/3 top-[40%] md:left-10 left-[30%] mt-5 rounded-3xl	h-[50px] w-[180px] bg-blue-700 text-white tracking-widest">SEE PLANS</button>
       </div>
 
       <div className="rating-section flex flex-col items-center justify-center bg-coolGray-200 h-[250px] gap-2 fontCera">
         <span className="text-[32px] font-bold">
-          Our customers think we're <span className="text-[32px] text-[#00a0df]">EXCELLENT</span>
+          Our customers think we're <span className="text-[32px] text-[#00a0df] text-center m-auto md:inline block">EXCELLENT</span>
         </span>
         <div>
           <img src="https://media.blueapron.com/assets/registration/homepage/appstore_ratings_icon.svg" className="inline-block" /> <span className="font-bold text-4xl text-[#303236] align-middle leading-3">4.7</span>
