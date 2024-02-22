e6c79b8c84687b2ea5541e0a1f19f27d921d0b87
MeteBely <metehanmuradoglu17@gmail.com>
2024-02-22 23:26:11 +0300

    build your plan componenti res yapımı

--- src/components/BuildYourPlan.jsx
+++ src/components/BuildYourPlan.jsx
@@ -18,24 +18,24 @@ const BuildYourPlan = () => {
   };
 
   return (
-    <div className="w-[781px] m-auto border border-[#d3d5db]  px-[20px] pb-4 rounded-[8px] flex flex-row items-center justify-between h-[250px] my-12">
-      <div className="w-[45%]">
-        <h1 className="mt-[12px] mb-4 fontChronicle text-[30px] tracking-[.5px] text-[#002684] ">Build your Plan</h1>
+    <div className="w-auto px-0 min-[600px]:w-[781px] min-[600px]:px-[20px] m-auto border border-[#d3d5db] pb-4 rounded-[8px] flex flex-row items-center justify-between h-[250px] my-12">
+      <div className="w-[255px] min-[600px]:w-[45%]">
+        <h1 className="mt-[12px] mb-4 fontChronicle text-[28px] tracking-[.2px] min-[600px]:text-[30px] min-[600px]:tracking-[.5px] text-[#002684] ">Build Your Plan</h1>
         <div className="fontCera text-[15px] leading-[1.2] mt-[13px] mb-[25px] text-[#6a6d75]">You can update your preferences and skip, pause, or cancel at any time.*</div>
         <div className="flex flex-row items-center justify-center">
-          <div className="flex flex-col justify-center items-start mr-[60px]">
+          <div className="flex flex-col justify-center items-start mr-[40px] min-[600px]:mr-[60px]">
             <span className="text-[#303236] pt-4 text-[20px] fontCera font-semibold text-left">$12.49</span>
-            <p className="fontCera text-[14px] text-[#696d75]  text-left">per serving</p>
+            <p className="fontCera text-[14px] text-[#696d75] text-left h-[45px]">per serving</p>
           </div>
-          <div className="flex flex-col justify-center items-start mr-[60px]">
+          <div className="flex flex-col justify-center items-start mr-[40px] min-[600px]:mr-[60px]">
             <span className="text-[#303236] pt-4 text-[20px] fontCera font-semibold text-left">$10.99</span>
-            <p className="fontCera text-[14px] text-[#696d75] text-left">shipping</p>
+            <p className="fontCera text-[14px] text-[#696d75] text-left h-[45px]">shipping</p>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-end justify-center w-[50%] gap-4 h-[225px] relative">
-        <div className="flex flex-row items-start justify-center gap-[34px]">
-          <div className="my-auto fontCera text-[18px] text-[#6a6d75] pb-[5px]">Number of servings</div>
+        <div className="flex flex-row items-start justify-center gap-[17px] min-[600px]:gap-[34px]">
+          <div className="my-auto fontCera text-[15px] min-[600px]:text-[18px] text-[#6a6d75] pb-[5px]">Number of servings</div>
           <ul
             className={`relative flex flex-row h-[38px] rounded-[58px] bg-[#f8f9fa] overflow-visible justify-center items-center ${
               activeNumberOfServingBtn === 'twoServingBtn'
@@ -55,8 +55,8 @@ const BuildYourPlan = () => {
             </li>
           </ul>
         </div>
-        <div className="flex flex-row items-start justify-center gap-[25px]">
-          <div className="my-auto fontCera text-[18px] text-[#6a6d75] leading-[1.06] pb-[5px]">
+        <div className="flex flex-row items-start justify-center gap-0 min-[600px]:gap-[25px]">
+          <div className="my-auto fontCera text-[15px] min-[600px]:text-[18px] text-[#6a6d75] leading-[1.06] pb-[5px]">
             Meals per
             <br />
             week
