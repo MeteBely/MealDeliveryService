ae664248346f1a21d737fe05e032d2e7cd9b1ce2
MeteBely <metehanmuradoglu17@gmail.com>
2024-02-22 23:25:52 +0300

    on the menu bottom component res yapım

--- src/components/OnTheMenuBottomSection.jsx
+++ src/components/OnTheMenuBottomSection.jsx
@@ -1,7 +1,7 @@
 const OnTheMenuBottomSection = () => {
   return (
     <footer>
-      <div className="w-[600px] m-auto text-center">
+      <div className="w-auto m-auto text-center">
         <button className="text-white bg-[#f26c29] border border-[#f26c29] px-[33px] rounded-[2px] tracking-[2px] mb-2 fontCera h-[48px] text-[15px]">SEE PLANS</button>
         <p className="mb-4 text-[#696d75] text-[18px] italic fontCera">Or…</p>
         <p className="mb-[12px] text-[#696d75] text-[18px] italic fontCera">
